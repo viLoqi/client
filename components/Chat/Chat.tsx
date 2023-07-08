12bb7331a87539664b47635c5af35e7c16a9f377
Jie Chen <jiechenmc@gmail.com>
2023-07-07 23:46:12 -0400

    add moment, proto message

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -3,11 +3,12 @@ import { useRouter } from 'next/router';
 
 import { firebaseAuth, firebaseStore } from '@/core/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { onValue, ref } from 'firebase/database';
-import { getFirestore, collection, } from 'firebase/firestore';
+import { getFirestore, collection, query, orderBy } from 'firebase/firestore';
 import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+import { KeyboardEvent } from 'react';
 
 import styles from '@/components/Chat/Chat.module.scss';
+import Message, { MessageElement } from './Message';
 
 interface chatProps {
   setCourse: any// Dispatch<SetStateAction<string>>
@@ -22,8 +23,10 @@ const Chat = ({ setCourse, setSectionName, courseName, sectionName }: chatProps)
   const [user, authLoading, authError] = useAuthState(firebaseAuth);
   const [msgs, setMsgs] = useState<JSX.Element[]>([]);
   const [message, setMessage] = useState('');
+  const chatBoxRef = useRef<HTMLDivElement>(null)
 
   console.log(user?.displayName);
+  console.log(user?.photoURL)
 
 
   if (course) {
@@ -33,8 +36,14 @@ const Chat = ({ setCourse, setSectionName, courseName, sectionName }: chatProps)
 
   const cname = courseName === '' ? 'CSE 101' : courseName;
   const sname = sectionName === '' ? '01-LEC' : sectionName;
-  const [collectionValue, collectionLoading, collectionError] = useCollection(collection(firebaseStore, `chats/${cname}/${sname}/room/messages`), { snapshotListenOptions: { includeMetadataChanges: true } });
+  const [collectionValue, collectionLoading, collectionError] = useCollection(query(collection(firebaseStore, `chats/${cname}/${sname}/room/messages`), orderBy("firstCreated", "desc")))
 
+  useEffect(() => {
+    // This will pin the chatbox to the bottom
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight
+    }
+  }, [message, collectionValue])
 
   const handleOnClick = () => {
     if (message)
@@ -42,24 +51,41 @@ const Chat = ({ setCourse, setSectionName, courseName, sectionName }: chatProps)
         method: 'POST', body: JSON.stringify({
           'collectionPath': `chats/${courseName}/${sectionName}/room/messages`,
           'content': message,
-          'author': user?.displayName
+          'author': user?.displayName,
+          'authorPhotoURL': user?.photoURL,
         })
-      }).then(() => { console.log('Message Sent!'); }).catch(e => { console.log(e); });
+      }).then(() => {
+        console.log('Message Sent!');
+      }).catch(e => { console.log(e); });
   };
+
+  const handleOnKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleOnClick()
+    }
+  }
 
   return (
     <div className={styles.section} >
       <nav className={styles['left-nav-container']}>
 
       </nav>
-      <div className={styles['chat-container']}>
-        {collectionValue?.docs.map(e => {
-          const { content, author } = e.data();
-          return <div key={crypto.randomUUID()}>{content} by {author}</div>;
-        })}
-        <input onChange={e => setMessage(e.target.value)}></input>
-        <button onClick={handleOnClick}>Send</button>
+
+
+      <div className={styles['main']}>
+        <div className={styles['chat-container']} ref={chatBoxRef}>
+          {collectionValue?.docs.map(e => {
+            const currMsg = e.data() as MessageElement
+            return <Message key={crypto.randomUUID()} {...currMsg} />;
+          })}
+        </div>
+
+        <div className={styles['input-container']}>
+          <input onChange={e => setMessage(e.target.value)} onKeyDown={e => handleOnKeyDown(e)}></input>
+          <button onClick={handleOnClick} >Send</button>
+        </div>
       </div>
+
     </div >
   );
 };
---
+++ components/Chat/Message.tsx
@@ -0,0 +1,31 @@
+import moment from "moment"
+import { Timestamp } from "firebase/firestore"
+import styles from './Message.module.scss'
+
+export interface MessageElement {
+    author: string
+    authorPhotoURL: string
+    content: string
+    firstCreated: Timestamp
+    lastUpdated: Timestamp
+}
+
+const Message = ({ author, authorPhotoURL, content, firstCreated, lastUpdated }: MessageElement) => {
+    const messageDisplayTime = moment().calendar(lastUpdated.toDate()).toString()
+
+    // if firstCreated != lastUpdated then that means the message was edited
+    return <div>
+        <div>
+            <img src={authorPhotoURL} />
+            <span className={styles["authorName"]}>
+                {author}
+            </span>
+            <span className={styles["messageDisplayTime"]}>
+                {messageDisplayTime}
+            </span>
+        </div>
+        {content}
+    </div >;
+}
+
+export default Message;
