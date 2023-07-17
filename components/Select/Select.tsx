cbee6186927089424ebea55442da83e973caccce
Jie Chen <jiechenmc@gmail.com>
2023-07-16 23:00:16 -0400

    bootleg join mechanism is complete

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -1,25 +1,31 @@
 import { useRouter } from 'next/router';
-import { firebaseStore, doc, useDocument, SectionInfoDoc } from '@/core/firebase';
+import { firebaseStore, doc, useDocument, SectionInfoDoc, useAuthState, firebaseAuth } from '@/core/firebase';
 import styles from './Select.module.scss';
 import Link from 'next/link';
 import DistrChart from './DistrChart';
+import { User } from 'firebase/auth';
 
 interface SelectProps {
     courseName: any
     setCourseName: any
 }
 
-const redirectSideEffect = () => {
+const redirectSideEffect = (courseName: string, section: string, user: User) => {
     // Add this user to the list of online users for a chat room.
     // this can honestly be a cloud function
-    console.log("Invoking CF to add user to list of online users")
+    return () => {
+        console.log("Invoking CF to add user to list of online users")
+        fetch(`${process.env.NEXT_PUBLIC_FIREBASE_FUNCTION_UPDATE_PRESENCE}?name=${user?.displayName}&course=${courseName}&section=${section}&photoURL=${user?.photoURL}`)
+    }
 }
 
 const Select = ({ courseName, setCourseName }: SelectProps) => {
     const router = useRouter();
     const { course } = router.query;
     // This document contains the sections for a particular course
-    const [sectionDocument, isDocLoad, IsDocError] = useDocument(doc(firebaseStore, `/chats/${courseName === '' ? 'CSE101' : courseName}`));
+    const [sectionDocument, _isDocLoading, _docLoadErr] = useDocument(doc(firebaseStore, `/chats/${courseName === '' ? 'CSE101' : courseName}`));
+    const [user, _isUserLoading, _userLoadError] = useAuthState(firebaseAuth)
+
 
     const sections = sectionDocument?.data()?.sections!
 
@@ -29,7 +35,7 @@ const Select = ({ courseName, setCourseName }: SelectProps) => {
     return (<div className={styles.container}>
         {sections !== undefined ? sections.map((e: SectionInfoDoc) => {
             return <div key={e.sec_id} className={styles['sectionCard']}>
-                <Link key={crypto.randomUUID()} href={`/chat?course=${courseName}&section_id=${e.sec_id}`} onClick={redirectSideEffect}>{e.sec_id} by {e.sec_ins}</Link>
+                <Link key={crypto.randomUUID()} href={`/chat?course=${courseName}&section_id=${e.sec_id}`} onClick={redirectSideEffect(courseName, e.sec_id, user)}>{e.sec_id} by {e.sec_ins}</Link>
                 <DistrChart courseName={courseName} instructor={e.sec_ins} />
             </div>
 
