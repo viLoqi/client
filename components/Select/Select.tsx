29cc9f7dd426d90277ee599220c84c387e034048
Jie Chen <jiechenmc@gmail.com>
2023-07-17 10:41:05 -0400

    use of cloud function to update presence

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -1,22 +1,14 @@
 import { useRouter } from 'next/router';
-import { firebaseStore, doc, useDocument, SectionInfoDoc, useAuthState, firebaseAuth } from '@/core/firebase';
+import { firebaseStore, doc, useDocument, SectionInfoDoc, useAuthState, firebaseAuth, firebaseApp } from '@/core/firebase';
 import styles from './Select.module.scss';
 import Link from 'next/link';
 import DistrChart from './DistrChart';
 import { User } from 'firebase/auth';
-
+import { useHttpsCallable } from 'react-firebase-hooks/functions';
+import { getFunctions } from 'firebase/functions';
 interface SelectProps {
     courseName: any
     setCourseName: any
-}
-
-const redirectSideEffect = (courseName: string, section: string, user: User) => {
-    // Add this user to the list of online users for a chat room.
-    // this can honestly be a cloud function
-    return () => {
-        console.log("Invoking CF to add user to list of online users")
-        fetch(`${process.env.NEXT_PUBLIC_FIREBASE_FUNCTION_UPDATE_PRESENCE}?name=${user?.displayName}&course=${courseName}&section=${section}&photoURL=${user?.photoURL}`)
-    }
 }
 
 const Select = ({ courseName, setCourseName }: SelectProps) => {
@@ -26,6 +18,10 @@ const Select = ({ courseName, setCourseName }: SelectProps) => {
     const [sectionDocument, _isDocLoading, _docLoadErr] = useDocument(doc(firebaseStore, `/chats/${courseName === '' ? 'CSE101' : courseName}`));
     const [user, _isUserLoading, _userLoadError] = useAuthState(firebaseAuth)
 
+    const [executeCallable, executing, error] = useHttpsCallable(
+        getFunctions(firebaseApp),
+        'update_presence',
+    );
 
     const sections = sectionDocument?.data()?.sections!
 
@@ -35,8 +31,11 @@ const Select = ({ courseName, setCourseName }: SelectProps) => {
     return (<div className={styles.container}>
         {sections !== undefined ? sections.map((e: SectionInfoDoc) => {
             return <div key={e.sec_id} className={styles['sectionCard']}>
-                <Link key={crypto.randomUUID()} href={`/chat?course=${courseName}&section_id=${e.sec_id}`} onClick={redirectSideEffect(courseName, e.sec_id, user)}>{e.sec_id} by {e.sec_ins}</Link>
+                <Link key={crypto.randomUUID()} href={`/chat?course=${courseName}&section_id=${e.sec_id}`} onClick={async () => {
+                    await executeCallable({ uid: user?.uid, name: user?.displayName, course: courseName, section: e.sec_id, photoURL: user?.photoURL });
+                }}>{e.sec_id} by {e.sec_ins}</Link>
                 <DistrChart courseName={courseName} instructor={e.sec_ins} />
+
             </div>
 
         }) : <></>}
